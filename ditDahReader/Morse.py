refactored Play class out to isolate sounddevice to see if we can fix clicking from underflow issue

ditDahReader/Morse.py
@@ -2,6 +2,7 @@ import math
 import re
 
 from .morse_dict import *
+from .Play import *
 from .Tone import *
 
 
@@ -40,6 +41,7 @@ class Morse:
             int(self.word_space * self.ms * self.sample_rate + 0.5))
         self.gain = _gain
         self.hzTone = _hzTone
+        self.p = Play(_sample_rate)
 
     def translate(self, text):
         """
@@ -86,8 +88,4 @@ class Morse:
             if c == " ":
                 w = np.append(w, self.word_spaceZ)
 
-        wav_wave = np.array(self.gain * w, dtype=np.int16)
-        sd.play(wav_wave, self.sample_rate)
-        status = sd.wait()
-        if status:
-            print('Error: ' + str(status))
+        self.p.play(w, self.gain)

ditDahReader/Tone.py
@@ -5,7 +5,8 @@
 import math
 
 import numpy as np
-import sounddevice as sd
+
+from .Play import *
 
 ms = math.pow(10, -3)
 
@@ -23,8 +24,8 @@ class Tone:
         self.tf = _tone_freq
         self.attack = _attack
         self.gain = _gain
-        sd.default.samplerate = self.fs
         self.raisedCosine()
+        self.p = Play(_sample_freq)
 
     def raisedCosine(self):
         self.rc = (1 + np.cos(np.arange(-math.pi,
@@ -48,8 +49,4 @@ class Tone:
 
     def play(self):
         """play the tone, needs to be created with createTone"""
-        wav_wave = np.array(self.gain * self.tone, dtype=np.int16)
-        sd.play(wav_wave)
-        status = sd.wait()
-        if status:
-            print('Error: ' + str(status))
+        p.play(self.tone, self.gain)